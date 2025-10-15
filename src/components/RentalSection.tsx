62ef94405e13f449f001f2a730fd286e2ac94498
Sourabh Mishra <51873245+OxSourabh@users.noreply.github.com>
2025-10-15 16:32:46 +0530

    tst3.1

--- src/components/RentalSection.tsx
+++ src/components/RentalSection.tsx
@@ -226,7 +226,7 @@ const RentalSection = () => {
                 <button className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg hover:bg-blue-700 transition-colors font-medium">
                   Request Rental Quote
                 </button>
-</a>
+              </a>
               </div>
             </div>
           ))}
